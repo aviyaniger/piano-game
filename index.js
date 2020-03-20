b9da79df2078c9f91cf9eec67166dd214b3ad4a2
Avi Yaniger <avi.yaniger@gmail.com>
2020-03-20 14:58:46 +0200

    added second octave

--- index.js
+++ index.js
@@ -1,78 +1,149 @@
-const lowC = document.querySelector("#low-c");
-lowC.addEventListener("click", function() {
-    const audioLowC = new Audio("Notes/00do.mp3");
-    audioLowC.play();
+const c0 = document.querySelector("#c0");
+c0.addEventListener("click", function() {
+    const audioC0 = new Audio("Notes/00do.mp3");
+    audioC0.play();
 });
 
-const db = document.querySelector("#db");
-db.addEventListener("click", function() {
-    const audioDb = new Audio("Notes/00re_b.mp3");
-    audioDb.play();
+const db0 = document.querySelector("#db0");
+db0.addEventListener("click", function() {
+    const audioDb0 = new Audio("Notes/00re_b.mp3");
+    audioDb0.play();
 });
 
-const d = document.querySelector("#d");
-d.addEventListener("click", function() {
-    const audioD = new Audio("Notes/00re.mp3");
-    audioD.play();
+const d0 = document.querySelector("#d0");
+d0.addEventListener("click", function() {
+    const audioD0 = new Audio("Notes/00re.mp3");
+    audioD0.play();
 });
 
-const eb = document.querySelector("#eb");
-eb.addEventListener("click", function() {
-    const audioEb = new Audio("Notes/00mi_b.mp3");
-    audioEb.play();
+const eb0 = document.querySelector("#eb0");
+eb0.addEventListener("click", function() {
+    const audioEb0 = new Audio("Notes/00mi_b.mp3");
+    audioEb0.play();
 });
 
-const e = document.querySelector("#e");
-e.addEventListener("click", function() {
-    const audioE = new Audio("Notes/00mi.mp3");
-    audioE.play();
+const e0 = document.querySelector("#e0");
+e0.addEventListener("click", function() {
+    const audioE0 = new Audio("Notes/00mi.mp3");
+    audioE0.play();
 });
 
-const f = document.querySelector("#f");
-f.addEventListener("click", function() {
-    const audioF = new Audio("Notes/00fa.mp3");
-    audioF.play();
+const f0 = document.querySelector("#f0");
+f0.addEventListener("click", function() {
+    const audioF0 = new Audio("Notes/00fa.mp3");
+    audioF0.play();
 });
 
-const gb = document.querySelector("#gb");
-gb.addEventListener("click", function() {
-    const audioGb = new Audio("Notes/00sol_b.mp3");
-    audioGb.play();
+const gb0 = document.querySelector("#gb0");
+gb0.addEventListener("click", function() {
+    const audioGb0 = new Audio("Notes/00sol_b.mp3");
+    audioGb0.play();
 });
 
-const g = document.querySelector("#g");
-g.addEventListener("click", function() {
-    const audioG = new Audio("Notes/00sol.mp3");
-    audioG.play();
+const g0 = document.querySelector("#g0");
+g0.addEventListener("click", function() {
+    const audioG0 = new Audio("Notes/00sol.mp3");
+    audioG0.play();
 });
 
-const ab = document.querySelector("#ab");
-ab.addEventListener("click", function() {
-    const audioAb = new Audio("Notes/00la_b.mp3");
-    audioAb.play();
+const ab0 = document.querySelector("#ab0");
+ab0.addEventListener("click", function() {
+    const audioAb0 = new Audio("Notes/00la_b.mp3");
+    audioAb0.play();
 });
 
-const a = document.querySelector("#a");
-a.addEventListener("click", function() {
-    const audioA = new Audio("Notes/00la.mp3");
-    audioA.play();
+const a0 = document.querySelector("#a0");
+a0.addEventListener("click", function() {
+    const audioA0 = new Audio("Notes/00la.mp3");
+    audioA0.play();
 });
 
-const bb = document.querySelector("#bb");
-bb.addEventListener("click", function() {
-    const audioBb = new Audio("Notes/00si_b.mp3");
-    audioBb.play();
+const bb0 = document.querySelector("#bb0");
+bb0.addEventListener("click", function() {
+    const audioBb0 = new Audio("Notes/00si_b.mp3");
+    audioBb0.play();
 });
 
-const b = document.querySelector("#b");
-b.addEventListener("click", function() {
-    const audioB = new Audio("Notes/00si.mp3");
-    audioB.play();
+const b0 = document.querySelector("#b0");
+b0.addEventListener("click", function() {
+    const audioB0 = new Audio("Notes/00si.mp3");
+    audioB0.play();
 });
 
-const highC = document.querySelector("#high-c");
-highC.addEventListener("click", function() {
-    const audioHighC = new Audio("Notes/01do.mp3");
-    audioHighC.play();
+const c1 = document.querySelector("#c1");
+c1.addEventListener("click", function() {
+    const audioC1 = new Audio("Notes/01do.mp3");
+    audioC1.play();
 });
 
+const db1 = document.querySelector("#db1");
+db1.addEventListener("click", function() {
+    const audioDb1 = new Audio("Notes/01re_b.mp3");
+    audioDb1.play();
+});
+
+const d1 = document.querySelector("#d1");
+d1.addEventListener("click", function() {
+    const audioD1 = new Audio("Notes/01re.mp3");
+    audioD1.play();
+});
+
+const eb1 = document.querySelector("#eb1");
+eb1.addEventListener("click", function() {
+    const audioEb1 = new Audio("Notes/01mi_b.mp3");
+    audioEb1.play();
+});
+
+const e1 = document.querySelector("#e1");
+e1.addEventListener("click", function() {
+    const audioE1 = new Audio("Notes/01mi.mp3");
+    audioE1.play();
+});
+
+const f1 = document.querySelector("#f1");
+f1.addEventListener("click", function() {
+    const audioF1 = new Audio("Notes/01fa.mp3");
+    audioF1.play();
+});
+
+const gb1 = document.querySelector("#gb1");
+gb1.addEventListener("click", function() {
+    const audioGb1 = new Audio("Notes/01sol_b.mp3");
+    audioGb1.play();
+});
+
+const g1 = document.querySelector("#g1");
+g1.addEventListener("click", function() {
+    const audioG1 = new Audio("Notes/01sol.mp3");
+    audioG1.play();
+});
+
+const ab1 = document.querySelector("#ab1");
+ab1.addEventListener("click", function() {
+    const audioAb1 = new Audio("Notes/01la_b.mp3");
+    audioAb1.play();
+});
+
+const a1 = document.querySelector("#a1");
+a1.addEventListener("click", function() {
+    const audioA1 = new Audio("Notes/01la.mp3");
+    audioA1.play();
+});
+
+const bb1 = document.querySelector("#bb1");
+bb1.addEventListener("click", function() {
+    const audioBb1 = new Audio("Notes/01si_b.mp3");
+    audioBb1.play();
+});
+
+const b1 = document.querySelector("#b1");
+b1.addEventListener("click", function() {
+    const audioB1 = new Audio("Notes/01si.mp3");
+    audioB1.play();
+});
+
+const c2 = document.querySelector("#c2");
+c2.addEventListener("click", function() {
+    const audioC2 = new Audio("Notes/02do.mp3");
+    audioC2.play();
+});
